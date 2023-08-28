2e73bf7c6b6ae4ce7aa5b0dc32e053abeb4186b0
Matt30Curia <matteocu10@hotmail.com>
2023-08-28 11:42:37 +0200

    Update AboutUs.js

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -4,7 +4,15 @@ import aboutUs from '../asset/about-us.png'
 function AboutUs(){
   return (
     <div className="background--aboutus">
-      <img src={aboutUs} />
+      <img src={aboutUs} alt="" />
+      <h1 className="section-name--aboutus">About us</h1>
+      <h2 className="title--aboutus">
+        We provide quality coffee, and ready to deliver.
+      </h2>
+      <p className="content--aboutus">
+        We are a company that makes and distributes delicious drinks. our main
+        product is made with a secret recipe and available in stores worldwide.
+      </p>
     </div>
   );
 }
