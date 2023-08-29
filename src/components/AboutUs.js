c2d3ef146069cae468d18b43d0a637fa02030e0a
Matt30Curia <matteocu10@hotmail.com>
2023-08-29 11:30:20 +0200

    init style

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,21 +1,25 @@
-import aboutUs from '../asset/about-us.png'
+import aboutUs from "../asset/about-us.png";
+import "../style/AboutUS.css";
 
-
-function AboutUs(){
+function AboutUs() {
   return (
-    <div className="background--aboutus">
-      <img src={aboutUs} alt="" />
-      <h1 className="section-name--aboutus">About us</h1>
-      <h2 className="title--aboutus">
-        We provide quality coffee, and ready to deliver.
-      </h2>
-      <p className="content--aboutus">
-        We are a company that makes and distributes delicious drinks. our main
-        product is made with a secret recipe and available in stores worldwide.
-      </p>
+    <div class="background--aboutus">
+      <img src={aboutUs} alt="" class="img--aboutus" />
+      <div class="text-container">
+        <h1 class="section-name--aboutus">About us</h1>
+        <div class="text-content">
+          <h2 class="title--aboutus">
+            We provide quality coffee, and ready to deliver.
+          </h2>
+          <p class="content--aboutus">
+            We are a company that makes and distributes delicious drinks. Our
+            main product is made with a secret recipe and available in stores
+            worldwide.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
 
-
-export default AboutUs
+export default AboutUs;
